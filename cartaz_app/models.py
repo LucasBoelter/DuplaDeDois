a400f2bbfd6ac82da57c9adab47a773c49b54396
LucasBoelter <boelter.luc4as@gmail.com>
2023-07-19 21:39:49 -0300

    ajustes coluna bd e pagina admin

--- cartaz_app/models.py
+++ cartaz_app/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from datetime import datetime
 
 class Cartaz(models.Model):
     titulo = models.CharField(max_length=100)
-    sinopse = models.CharField(max_length=400)
+    subtitulo = models.CharField(max_length=100, null=True)
+    data_lancamento = models.DateField(default=datetime.now)
+    sinopse = models.TextField(max_length=400)
     link_trailer = models.CharField(max_length=200)
     imagem = models.ImageField(upload_to='imagens/%Y/%m/%d')
+    em_cartaz = models.BooleanField(default=False)
 
     def __str__(self):
         return self.titulo
