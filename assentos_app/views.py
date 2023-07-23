ece019417866ea7789a6448b082b2451593f2343
LucasBoelter <boelter.luc4s@gmail.com>
2023-07-22 22:10:53 -0300

    puxando filme e assentos ate a pagina de pagamento

--- assentos_app/views.py
+++ assentos_app/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render
+from cartaz_app.models import Cartaz
 
 def view_index(request):
     array_fileiras = ['A', 'B', 'C', 'D']
     cadeiras_por_fileira = 7
 
+    codigo_filme = request.GET['codigoFilme']
+    print('filme: ', codigo_filme)
+
     context = {
         'fileiras' : array_fileiras,
-        'quant_cadeiras' : range(1, cadeiras_por_fileira)
+        'quant_cadeiras' : range(1, cadeiras_por_fileira),
+        'codigo_filme' : codigo_filme,
     } 
 
-    
     return render(request, "assentos_app/paginas/index.html", context)
 
 def view_pag(request):
-    buttons_clicked = request.GET.get('finalizar', '').split(',')
-    print(buttons_clicked)
-    return render (request, 'assentos_app/paginas/pagamento.html', context={})
+
+    lugares_selecionados = request.GET.get('lugaresSelecionados[]').split(',')
+    codigo_filme = request.GET.get('codigoFilme')
+    context = {
+        'lugares_selecionados' : lugares_selecionados,
+        'codigo_filme' : codigo_filme
+    }
+    return render (request, 'assentos_app/paginas/pagamento.html', context)
---
+++ static/global/js/script.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+    let lugares_selecionados = []
+
+    const assentos = document.querySelectorAll('.assento');
+    
+    const seleciona_lugar = ({target}) => {
+        // Colocar a interação do botão aqui dentro, oque ele deve fazer
+        console.log(target);
+        let assentoId = target.getAttribute('id')
+        let inputLugaresSelecionados = document.getElementById('lugaresSelecionados');
+
+        // Alterna entre as classes que dão a cor para o assento
+        target.classList.toggle("btn-danger");
+        target.classList.toggle("btn-dark");
+
+        // Verifica se o assento foi selecionado pelo usuário
+        if(lugares_selecionados.includes(assentoId)){
+            //Caso já tenha sido selecionado pelo usuário, será removido do array
+            let index = lugares_selecionados.indexOf(assentoId);
+            lugares_selecionados.splice(index, 1);
+        }else{
+            //Caso não tenha sido selecionado pelo usuário, será adicionado ao array
+            lugares_selecionados.push(assentoId);
+        }
+
+        inputLugaresSelecionados.value = lugares_selecionados;
+        
+    }
+
+    //Atribui o evento de click a todos os botões de assento
+    assentos.forEach((element) => {
+        element.addEventListener('click', seleciona_lugar);
+    })
+});
