b84a94bb44480f0a7788ac891a40963a7044b6e4
larisarapio <larissa.sarapio@gmail.com>
2023-07-25 07:19:10 -0300

    imagem atualizadaa

--- usuario_app/urls.py
+++ usuario_app/urls.py
@@ -8,13 +8,13 @@ urlpatterns = [
     path('', views.view_index, name="index"),
     path('login/', views.view_login, name="login"),
     path('cadastro/', views.view_cadastro, name="cadastro"),
-    path('admin/', admin.site.urls, name='admin'),
     path('logout/', views.logout, name="logout"),
+
+    path('admin/', admin.site.urls, name='admin'),
+
     path('perfil/', views.view_perfil, name='perfil'),
     path('adicionar-imagem/', views.view_add_imagem, name="adicionar_imagem"),
-    path('editar-imagem/', views.view_edt_imagem, name="editar_imagem"), # talvez
-    path('apagar-imagem/', views.view_apg_imagem, name="apagar_imagem"), # talvez
+    # path('editar-imagem/', views.view_edt_imagem, name="editar_imagem"),
     path('editar-imagem/<int:id_url>/', views.view_edt_imagem, name="editar_imagem"),
     path('apagar-imagem/<int:id_url>/', views.view_apg_imagem, name="apagar_imagem"),
-    
 ]
