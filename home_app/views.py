dd472375039d15f895e5cf632f05af0af2082cdf
LucasBoelter <boelter.luc4as@gmail.com>
2023-07-25 19:23:06 -0300

    ajustes menu

--- home_app/views.py
+++ home_app/views.py
@@ -4,8 +4,3 @@ from . import models
 def view_index(request):
     imagens = models.Home.objects.all()
     return render(request, "home_app/paginas/index.html", context={'imagens': imagens})
-
-#tentando puxar os dados do bd
-# def carrosel(request):
-#     imagens = models.Home.objects.all()
-#     return render(request, 'home_app/paginas/index.html', {'imagens': imagens})
